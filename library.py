679470fbc246e705799ee6e2f18f29b316f78a98
Collin Brown <coolinxx@gmail.com>
2022-12-12 16:01:35 -0500

    Added SConstript to src folder

--- library.py
+++ library.py
@@ -7,3 +7,28 @@ from SCons.Script import (AlwaysBuild, builder, COMMAND_LINE_TARGETS, Default, D
 from colorama import Fore
 
 print('<<<<<<<<<<' + 'Adding CANboot Bootloader' + '>>>>>>>>>>')
+
+# Output Directory
+
+# Common Command Definitions
+
+# Source Files
+
+# Default Compiler Flags
+
+# Default Targets
+
+# Include Board Specific Scons File
+
+Export('env')
+SConscript('SConscript')
+
+# Main Build Rules
+
+# Deployer Build Rules
+
+# Compile Time Requests
+
+# Auto Generation of "board/ include file link"
+
+#Generic Rules
